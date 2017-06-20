6d9eebcb8f96d1380c0802b535a86fa2ce0b9b3e
Craig <craigvandervelden@gmail.com>
2017-06-20 16:41:26 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+T = 450;
+# Operating Temperature
+B = 1 / (8.314 * T / 1000)
+
+#  Emin / Emax are in kJ / mol
+Emin = 10.069
+Emax = 74.613
+
+# Polynomial power for poly distribution
+a = 3
+
+#
